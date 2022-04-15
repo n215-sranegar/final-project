90391200c72e57a22c77844959d6994e2f16b753
sranegar <stephanie.ranegar@gmail.com>
2022-04-14 22:27:39 -0400

    Blog story page complete and loaded dynamically into Modal.js

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ function route() {
     let pageContent = pageName + "Content";
 
     if (pageName == "") {
-        pageContent = "";
+        pageContent = "homeContent";
     }
 
     MODEL.modelPageName(pageContent);
--- model/model.js
+++ model/model.js
@@ -314,6 +314,24 @@ var blogContent = `  <div class="blog-page-container">
 </div>
 </div>`
 
+var blog1Content = ` <div class="blog-story-page">
+<div class="blog-story-wrapper">
+  <div class="blog-story-header">
+    <div class="story-date-wrapper">
+      <p>06</p>
+      <span><p>JUN</p></span>
+    </div>
+  </div>
+  <div class="blog-story-text-wrapper">
+  <h1>Header #1:</h1>
+  <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
+  <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+  <h1>Header #2:</h1>
+  <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+</div>
+</div>
+</div>`
+
 //This function is being called from the app.js and it has the page name
 export function modelPageName(pgName) {
     if (pgName == "homeContent") {
